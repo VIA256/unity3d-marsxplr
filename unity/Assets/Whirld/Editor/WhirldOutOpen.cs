018f8990400b7d87a0559de780ae47c98699aded
VIA256 <via256@mail.com>
2026-07-31 00:37:20 -0700

    replace Whirld/Editor js with cs stubs

---
+++ unity/Assets/Whirld/Editor/WhirldOutOpen.cs
@@ -0,0 +1,27 @@
+﻿/*
+
+
+
+
+//**************************************************************************************************
+//*********************************** Whirld - by Aubrey Falconer **********************************
+//**** http://AubreyFalconer.com **** http://www.unifycommunity.com/wiki/index.php?title=Whirld ****
+//**************************************************************************************************
+
+class WhirldOutOpen extends System.Object {
+	
+	var whirldOut : WhirldOut;
+	
+	function WhirldOutOpen(out : WhirldOut) {
+		
+		//Init
+		whirldOut = out;
+		
+	}
+	
+}
+
+
+
+
+ */
