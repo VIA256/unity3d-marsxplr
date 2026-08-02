d8b2834b5e126eac1a0a9cdb8ba34c37156c726d
VIA256 <via256@mail.com>
2026-08-02 00:49:09 -0700

    set to version 4.1

--- unity/Assets/Martian Assets/Scripts/Game/GameData.cs
+++ unity/Assets/Martian Assets/Scripts/Game/GameData.cs
@@ -4,8 +4,7 @@ using UnityEngine;
 [Serializable]
 public class GameData : MonoBehaviour
 {
-	//public static float gameVersion = 4.0f;
-	public static SemVer gameVersion = new SemVer("4.0.3");
+	public static SemVer gameVersion = new SemVer("4.1");
 	public static float serverVersion = 0.2f;
 	public static string gameName = "marsxplr";
 	public static string userName = "";
