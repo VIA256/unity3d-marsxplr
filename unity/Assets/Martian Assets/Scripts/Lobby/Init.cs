48d0f59bbcc0f2f944c484786fa6142e186e687c
VIA256 <via256@mail.com>
2026-08-03 20:47:56 -0700

    set player settings for pc/mac/linux
    configure icons,
    tell player to prompt for resolution on startup,
    disable automaticaly setting the resolution in Init.cs

--- unity/Assets/Martian Assets/Scripts/Lobby/Init.cs
+++ unity/Assets/Martian Assets/Scripts/Lobby/Init.cs
@@ -6,28 +6,6 @@ public class Init : MonoBehaviour
 {
 	public GUIText txt;
 
-	//----------------------
-	//	temporary fix for texture/font rendering issues on super high res moniters
-	//  resolution is capped to 1920x1080
-	public void Start()
-	{
-		int wnew = -1;
-		int hnew = -1;
-		foreach(Resolution r in Screen.resolutions)
-		{
-			if(
-				r.width > 1920 || r.width < wnew ||
-				r.height > 1080 || r.height < hnew)
-			{
-				continue;
-			}
-			wnew = r.width;
-			hnew = r.height;
-		}
-		Screen.SetResolution(wnew, hnew, Screen.fullScreen);
-	}
-	//----------------------
-
 	public void Update()
     {
 		float i = Application.GetStreamProgressForLevel(Application.loadedLevel + 1);
